ffb6f45fe0413af3540bfca509c1b8fba3004d0a
PersiLiao <xiangchu.liao@gmail.com>
2020-08-15 10:03:50 +0800

    fixed A 0 file was generated when an error occurred.

--- install_composer_switcher.sh
+++ install_composer_switcher.sh
@@ -6,19 +6,12 @@ pushd ${workDirectory} > /dev/null
 workDirectory=$(pwd)
 
 cp "${workDirectory}/composer-switcher.sh" "/usr/local/bin/composer-switcher"
-
-if [ $? <> 0 ]; then
-    exit 1
-fi
-
 chmod +x "/usr/local/bin/composer-switcher"
 
-if [ $? <> 0 ]; then
-    exit 1
-fi
-
-if [ `uname` == "Darwin" ]; then
-    echo "\033[32m🍺 Installed the Composer switcher successfully! Enjoy :-)\033[0m"
-else
-    echo -e "\033[32m🍺 Installed the Composer switcher successfully! Enjoy :-)\033[0m"
+if [ $? == 0 ]; then
+    if [ `uname` == "Darwin" ]; then
+        echo "\033[32m🍺 Installed the Composer switcher successfully! Enjoy :-)\033[0m"
+    else
+        echo -e "\033[32m🍺 Installed the Composer switcher successfully! Enjoy :-)\033[0m"
+    fi
 fi
