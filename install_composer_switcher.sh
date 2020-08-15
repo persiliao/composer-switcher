b73504aaed3921c25e831bf46d381fa33b4224da
PersiLiao <xiangchu.liao@gmail.com>
2020-08-15 08:46:53 +0800

    fixed file permissions issues.

--- install_composer_switcher.sh
+++ install_composer_switcher.sh
@@ -6,7 +6,17 @@ pushd ${workDirectory} > /dev/null
 workDirectory=$(pwd)
 
 cp "${workDirectory}/composer-switcher.sh" "/usr/local/bin/composer-switcher"
+
+if [ $? <> 0 ]; then
+    exit 1
+fi
+
 chmod +x "/usr/local/bin/composer-switcher"
+
+if [ $? <> 0 ]; then
+    exit 1
+fi
+
 if [ `uname` == "Darwin" ]; then
     echo "\033[32m🍺 Installed the Composer switcher successfully! Enjoy :-)\033[0m"
 else
